85a9ef2f26d16805ab1d35530ef2a3229777430a
r33drichards <rwendt1337@gmail.com>
2025-03-21 23:34:16 -0700

    s/cuse-agent/surf/g

--- components/repo-banner.tsx
+++ components/repo-banner.tsx
@@ -4,7 +4,7 @@ import { Separator } from "./ui/separator";
 import { cn } from "@/lib/utils";
 import { StarFilledIcon } from "@radix-ui/react-icons";
 
-const REPO_URL = "https://github.com/e2b-dev/cuse-agent";
+const REPO_URL = "https://github.com/e2b-dev/surf";
 
 export function RepoBanner() {
   return (
